cad0cd85f981cf87cb8a349730b3157f05b1db12
Pernthaler <sebastian@pernthaler.me>
2021-03-01 19:25:29 +0100

    Preload font only

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
     render: {
         bundleRenderer: {
             shouldPreload: (file, type) => {
-                return ["script", "style", "font"].includes(type);
+                return ["font"].includes(type);
             }
         }
     }
