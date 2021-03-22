d0a63a34f1fc66ab7c426cd8c10a715a663f2dad
Pernthaler <sebastian@pernthaler.me>
2021-03-22 12:49:47 +0100

    Add Storybook

--- src/pages/website.vue
+++ src/pages/website.vue
@@ -2,11 +2,13 @@
 {
   "en": {
     "title": "Website",
-    "subtitle": "This website was created with Vue.js and is open source."
+    "subtitle": "This website was created with Vue.js and is open source.",
+    "components": "Components"
   },
   "de": {
     "title": "Webseite",
-    "subtitle": "Diese Webseite wurde mit Vue.js erstellt und ist Open Source."
+    "subtitle": "Diese Webseite wurde mit Vue.js erstellt und ist Open Source.",
+    "components": "Komponenten"
   }
 }
 </i18n>
@@ -16,6 +18,8 @@
     <div class="col-12">
       <h1>{{ $t("title") }}</h1>
       <p>{{ $t("subtitle") }}</p>
+      <h2>{{ $t("components") }}</h2>
+      <iframe width="100%" height="500px" src="https://components.pernthaler.me"/>
     </div>
   </div>
 </template>
