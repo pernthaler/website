f18f230339efee0b42b954cd93d7b364e91a2676
Pernthaler <sebastian@pernthaler.me>
2021-06-02 00:30:46 +0200

    Add three

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,13 +13,63 @@
   <div>
     <h1>Sebastian Pernthaler</h1>
     <p>{{ $t("subtitle") }}</p>
+    <div id="three"/>
   </div>
 </template>
 
 <script>
+import * as THREE from "three";
+
 export default {
   created() {
     this.$store.commit("SET_TITLE", "");
+  },
+  data() {
+    return {
+      scene: null,
+      camera: null,
+      renderer: null,
+      cube: null
+    };
+  },
+  methods: {
+    init: function() {
+      const div = document.getElementById("three");
+
+      this.scene = new THREE.Scene();
+      this.camera = new THREE.PerspectiveCamera(75, div.clientWidth / div.clientHeight, 0.1, 1000);
+
+      this.renderer = new THREE.WebGLRenderer({ alpha: true });
+      this.renderer.setSize(div.clientWidth, div.clientHeight);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+      div.appendChild(this.renderer.domElement);
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
+      this.cube = new THREE.Mesh(geometry, material);
+      this.scene.add(this.cube);
+
+      this.camera.position.z = 5;
+    },
+    animate: function() {
+      requestAnimationFrame(this.animate);
+
+      this.cube.rotation.x += 0.01;
+      this.cube.rotation.y += 0.01;
+
+      this.renderer.render(this.scene, this.camera);
+    }
+  },
+  mounted() {
+    this.init();
+    this.animate();
   }
 }
 </script>
+
+<style lang="less" scoped>
+#three {
+  width: 500px;
+  height: 500px;
+}
+</style>
