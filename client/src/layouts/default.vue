9d6bc8787aa13de4b6e0ca04ce55c809216ba450
Pernthaler <sebastian@pernthaler.me>
2022-07-28 15:31:19 +0200

    Add Content

---
+++ client/src/layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+useHead({
+  title: "Sebastian Pernthaler"
+});
+</script>
+
+<template>
+  <slot/>
+</template>
+
+<style lang="less">
+html, body, #__nuxt {
+  height: 100%;
+}
+body {
+  margin: 0;
+  color: white;
+  background-color: black;
+}
+</style>
