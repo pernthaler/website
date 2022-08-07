c8cd369689afbdad1f1b523ca821c5b7049d1353
Pernthaler <sebastian@pernthaler.me>
2022-08-07 15:30:14 +0200

    Add Critters + Font

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,6 @@
 import { ViteSSG } from "vite-ssg/single-page";
 import "virtual:windi.css";
+import "@fontsource/noto-mono/index.css";
 import App from "./App.vue";
 
 export const createApp = ViteSSG(App);
