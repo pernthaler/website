6420b51f9bafdc62be82f416e11542d5cedbe48d
Pernthaler <sebastian@pernthaler.me>
2022-12-15 20:24:29 +0100

    Add wails

--- cmd/website/main.go
+++ cmd/website/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 
+	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/pernthaler/website/web"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 func main() {
@@ -18,7 +24,7 @@ func main() {
 	}
 
 	defer conn.Close()
-	//ip := conn.LocalAddr().(*net.UDPAddr).IP
+	ip := conn.LocalAddr().(*net.UDPAddr).IP
 
 	app := fiber.New()
 
@@ -27,22 +33,20 @@ func main() {
 		PathPrefix: "static",
 	}))
 
-	/*
-		go wails.Run(&options.App{
-			Title:     fmt.Sprintf("website [%s:8080]", ip),
-			Width:     1280,
-			Height:    1024,
-			MinWidth:  320,
-			MinHeight: 256,
-			AssetServer: &assetserver.Options{
-				Handler: adaptor.FiberApp(app),
-			},
-			Windows: &windows.Options{
-				DisableWindowIcon: true,
-				Theme:             windows.Dark,
-			},
-		})
-	*/
+	go wails.Run(&options.App{
+		Title:     fmt.Sprintf("website [%s:8080]", ip),
+		Width:     1280,
+		Height:    1024,
+		MinWidth:  320,
+		MinHeight: 256,
+		AssetServer: &assetserver.Options{
+			Handler: adaptor.FiberApp(app),
+		},
+		Windows: &windows.Options{
+			DisableWindowIcon: true,
+			Theme:             windows.Dark,
+		},
+	})
 
 	app.Listen(":8080")
 }
