395fe53c653969dd188e0d6546fd31f43a0ea1b8
Sebastian Pernthaler <sebastian@pernthaler.dev>
2023-07-22 15:23:58 +0200

    Update controller

--- src/main/groovy/dev/pernthaler/website/controllers/Index.groovy
+++ src/main/groovy/dev/pernthaler/website/controllers/Index.groovy
@@ -1,21 +1,23 @@
-package dev.pernthaler.website.pages
+package dev.pernthaler.website.controllers
 
 import dev.pernthaler.website.entities.Link
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("/")
 class Index {
 
     def links = [
             new Link("Contact", "mailto:sebastian@pernthaler.dev", false),
             new Link("LinkedIn", "https://www.linkedin.com/in/pernthaler/", true),
             new Link("GitHub", "https://github.com/pernthaler/website", true)
-    ].asImmutable()
+    ]
 
-    @GetMapping("/")
-    index(ModelMap model) {
+    @GetMapping
+    def getIndex(ModelMap model) {
         model["links"] = links
         "index"
     }
